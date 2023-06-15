1d8a1406fdffe102c8c6369babbdc7a5612320a1
prasopchokk091 <prasopchokk091@hotmail.com>
2023-06-15 11:36:05 +0700

    add structure repository

---
+++ shared/repository/master.ts
@@ -0,0 +1,13 @@
+class MasterModule  {
+    private RESOURCE = '/Master';
+  
+    // async prefix(credentials: ILoginInput): Promise<ILoginResponse> {
+    //   return await useCallApi().post({})
+    // }
+  
+    // async create(account: ICreateAccountInput): Promise<ICreateAccountResponse> {
+    //   return await this.call<ICreateAccountResponse>('POST', `${this.RESOURCE}/register`, account);
+    // }
+  }
+  
+  export default MasterModule;

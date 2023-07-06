cc22a49ae459758dcdd5d34fe53b1f184de824a3
prasopchokk091 <prasopchokk091@hotmail.com>
2023-07-07 01:47:30 +0700

    extend component

---
+++ components/order/compulsory/summary/PaymentStatus.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="status-list">
+    <figure class="status-icon">
+      <div class="icon qr warning"></div>
+    </figure>
+    <h4 class="title">ชำระเงินด้วย QR</h4>
+    <div class="status-item text-blue text-big">
+      <h5 class="topic">ยอดรวมที่ต้องชำระ</h5>
+      <p>8,000.00 บาท</p>
+    </div>
+
+    <div class="status-info">
+      <div class="notice-info">
+        <i class="fa-regular fa-circle-info"></i> ค่าธรรมเนียม 5 บาท
+      </div>
+      <div class="status-action">
+        <a class="btn" href="#" title="เปลี่ยนช่องทางการชำระเงิน"
+          >เปลี่ยนช่องทางการชำระเงิน</a
+        >
+      </div>
+    </div>
+  </div>
+
+  <div class="status-list">
+    <figure class="status-icon">
+      <div class="icon cards warning"></div>
+    </figure>
+    <h4 class="title">ชำระเงินด้วย บัตรเครดิต/บัตรเดบิต</h4>
+    <div class="status-item text-blue text-big">
+      <h5 class="topic">ยอดรวมที่ต้องชำระ</h5>
+      <p>8,000.00 บาท</p>
+    </div>
+
+    <div class="status-info">
+      <div class="status-action">
+        <a class="btn" href="#" title="เปลี่ยนช่องทางการชำระเงิน"
+          >เปลี่ยนช่องทางการชำระเงิน</a
+        >
+      </div>
+    </div>
+  </div>
+
+  <div class="status-list">
+    <figure class="status-icon">
+      <div class="icon pledge danger"></div>
+    </figure>
+    <h4 class="title">ชำระเงินด้วย วงเงินมัดจำ</h4>
+    <div class="status-item text-blue text-big">
+      <h5 class="topic">ยอดรวมที่ต้องชำระ</h5>
+      <p>8,000.00 บาท</p>
+    </div>
+    <div class="status-item">
+      <h5 class="topic">ยอดเงินในวงเงินมัดจำ</h5>
+      <p>500.00 บาท</p>
+    </div>
+
+    <div class="status-info">
+      <div class="notice-danger">
+        <i class="fa-regular fa-circle-xmark"></i> วงเงินมัดจำไม่เพียงพอ
+      </div>
+      <div class="status-action">
+        <a class="btn btn-primary" href="#" title="เติมเงินมัดจำ">เติมเงินมัดจำ</a>
+        <a class="btn" href="#" title="เปลี่ยนช่องทางการชำระเงิน"
+          >เปลี่ยนช่องทางการชำระเงิน</a
+        >
+      </div>
+    </div>
+  </div>
+</template>

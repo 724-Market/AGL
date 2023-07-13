32d1229b1c5a776c574bdfe88899e360dca04d55
Pathompong Sangkajit <p.inhumba@gmail.com>
2023-07-13 16:35:13 +0700

    New: Add Thanks page

--- components/elements/monito/Sidenav.vue
+++ components/elements/monito/Sidenav.vue
@@ -42,7 +42,7 @@
                                 <a class="nav-link" href="/payment/qr" title="QR Payment">QR Payment</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="/page/thanks" title="Thanks">Thanks</a>
+                                <a class="nav-link" href="/order/compulsory/thanks" title="Thanks">Thanks</a>
                             </li>
                         </ul>
                     </div>

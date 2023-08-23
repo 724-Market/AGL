edd9dccddfe76efc528463670b7f67b62a27ce50
Pathompong Sangkajit <p.inhumba@gmail.com>
2023-08-23 15:18:23 +0700

    Dev: Remove validate on login

--- components/elements/form/Username.vue
+++ components/elements/form/Username.vue
@@ -1,8 +1,13 @@
 <template>
-    <FormKit type="text" label="รหัสสมาชิก" name="username" placeholder="AM – – – – – – – –"
+    <FormKit type="text" label="รหัสสมาชิก" name="username" placeholder="AM – – – – – – – –" validation="required"
+        :validation-messages="{
+            required: 'กรุณาใส่รหัสสมาชิก'
+        }" autocomplete="off" />
+
+    <!-- <FormKit type="text" label="รหัสสมาชิก" name="username" placeholder="AM – – – – – – – –"
         validation="required|matches:/^[aA][mM][0-9]{8}$/|length:10" :validation-messages="{
             required: 'กรุณาใส่รหัสสมาชิก',
             matches: 'รูปแบบของรหัสสมาชิกไม่ถูกต้อง',
             length: 'รหัสสมาชิกควรมี 10 ตัวอักษร และขึ้นต้นด้วย AM'
-        }" autocomplete="off" />
+        }" autocomplete="off" /> -->
 </template>

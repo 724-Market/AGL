e41ffef9963cc24bb8236190ecbcbdcdbebc1f0a
Prasopchok <prasopchokk091@gmail.com>
2023-09-02 16:14:50 +0700

    map field in content datatable

--- components/order/history/grid/column.vue
+++ components/order/history/grid/column.vue
@@ -1,42 +1,73 @@
 <template>
-  <div v-if="props.field && props.field == 'order'">
-    #200300033675<time datetime="2023-05-05 08:08">2023-05-05 08:08</time>
+  <div v-if="props.field && props.field == 'order' && props.row">
+    #{{ props.row.OrderNo
+    }}<time datetime="2023-05-05 08:08">{{ props.row.CreateDate }}</time>
   </div>
-  <div v-if="props.field && props.field == 'amount'">645.21<span>ส่วนลด 0.00</span></div>
-  <div v-if="props.field && props.field == 'name'">
-    ปฐมพงศ์ สังคจิตต์<span>089-xxx-x478</span>
+  <div v-if="props.field && props.field == 'subject' && props.row">
+    {{ props.row.CarLicense }} {{ props.row.RedLicense != "" ? "(ป้ายแดง)" : ""
+    }}<span>{{ props.row.CarBrand }} {{ props.row.CarModel }} ปี </span>
   </div>
-  <div v-if="props.field && props.field == 'status'">
-    <div class="badge-dot badge">แบบร่าง</div>
-    <div class="badge-dot badge-warning">รอชำระเงิน</div>
-    <div class="badge-dot badge-warning">รับแจ้งงาน</div>
-    <div class="badge-dot badge-warning">กำลังจัดส่ง</div>
-    <div class="badge-dot badge-success">สำเร็จ</div>
-    <span
-      ><a class="attached" href="#" title="ดาวน์โหลดกรมธรรม์"
-        >กรมธรรม์ TR-4635-453-32-1</a
+  <div v-if="props.field && props.field == 'amount' && props.row">
+    {{ props.row.OrderAmount }}<span>ส่วนลด {{ props.row.DiscountAmount }}</span>
+  </div>
+  <div v-if="props.field && props.field == 'name' && props.row">
+    {{ props.row.FirstName }} {{ props.row.LastName
+    }}<span v-if="props.row.PhoneNumber != ''"
+      >{{ props.row.PhoneNumber
+      }}<!--089-xxx-x478--></span
+    >
+  </div>
+  <div v-if="props.field && props.field == 'status' && props.row">
+    <div class="badge-dot badge" v-if="props.row.Status == 'Draft'">แบบร่าง</div>
+    <div class="badge-dot badge-warning" v-else-if="props.row.Status == 'Pending'">
+      รอชำระเงิน
+    </div>
+    <div class="badge-dot badge-warning" v-else-if="props.row.Status == 'Process'">
+      รับแจ้งงาน
+    </div>
+    <div class="badge-dot badge-warning" v-else-if="props.row.Status == 'Delivery'">
+      กำลังจัดส่ง
+    </div>
+    <div class="badge-dot badge-success" v-else-if="props.row.Status == 'Success'">
+      สำเร็จ
+    </div>
+    <div class="badge-dot badge-danger" v-else-if="props.row.Status == 'Cancel'">
+      ไม่สำเร็จ
+    </div>
+    <div class="badge-dot badge-warning" v-else-if="props.row.Status == 'Unknow'">
+      ไม่มีสถานะ
+    </div>
+    <span v-if="props.row.IsPolicyFile"
+      ><a
+        class="attached"
+        target="_blank"
+        :href="props.row.PolicyURL"
+        title="ดาวน์โหลดกรมธรรม์"
+        >กรมธรรม์ {{ props.row.Policy }}</a
       ></span
     >
-    <div class="badge-dot badge-danger">ไม่สำเร็จ</div>
   </div>
-  <div v-if="props.field && props.field == 'meta'">
+  <div v-if="props.field && props.field == 'meta' && props.row" class="meta">
     <span
+      v-if="props.row.CreateType == 'ONLINE'"
       class="badge meta-online"
       data-bs-toggle="tooltip"
       data-bs-custom-class="meta-tooltip"
-      data-bs-title="ทำรายการแบบ online<br>โดย ปฐมพงศ์ สังคจิตต์"
+      :data-bs-title="`ทำรายการแบบ Online<br>โดย ${props.row.CreateUser}: ${props.row.AssuredFullName}`"
       data-bs-html="true"
       ><em>On</em></span
     >
     <span
+      v-else-if="props.row.CreateType == 'OFFLINE'"
       class="badge meta-offline"
       data-bs-toggle="tooltip"
       data-bs-custom-class="meta-tooltip"
-      data-bs-title="ทำรายการแบบ offline<br>โดย ปฐมพงศ์ สังคจิตต์"
+      :data-bs-title="`ทำรายการแบบ Offline<br>โดย ${props.row.CreateUser}: ${props.row.AssuredFullName}`"
       data-bs-html="true"
       ><em>Off</em></span
     >
     <span
+      v-if="props.row.JobType == 'NEW'"
       class="badge meta-new"
       data-bs-toggle="tooltip"
       data-bs-custom-class="meta-tooltip"
@@ -44,6 +75,7 @@
       ><em>N</em></span
     >
     <span
+      v-else
       class="badge meta-renew"
       data-bs-toggle="tooltip"
       data-bs-custom-class="meta-tooltip"
@@ -54,45 +86,35 @@
       class="badge meta-company"
       data-bs-toggle="tooltip"
       data-bs-custom-class="meta-tooltip"
-      data-bs-title="ทิพยประกันภัย"
-      ><em>TIP</em></span
+      :data-bs-title="`${props.row.CompanyCode} : ${props.row.CompanyName}`"
+      ><em>{{ props.row.CompanyCode }}</em></span
     >
     <span
-      class="badge meta-company"
-      data-bs-toggle="tooltip"
-      data-bs-custom-class="meta-tooltip"
-      data-bs-title="ไทยศรี"
-      ><em>ERGO</em></span
-    >
-    <span
+      v-if="props.row.OrderType == 'VOLUNTARY'"
       class="badge meta-product"
       data-bs-toggle="tooltip"
       data-bs-custom-class="meta-tooltip"
-      data-bs-title="พ.ร.บ."
-      ><em>พรบ</em></span
+      :data-bs-title="OrderClassType[props.row.OrderClassType].text"
+      ><em>{{ OrderClassType[props.row.OrderClassType].title }}</em></span
     >
     <span
+      v-else
       class="badge meta-product"
       data-bs-toggle="tooltip"
       data-bs-custom-class="meta-tooltip"
-      data-bs-title="ประกันภัย ประเภท 1"
-      ><em>ป.1</em></span
+      :data-bs-title="OrderType[props.row.OrderSubType].text"
+      ><em>{{ OrderType[props.row.OrderSubType].title }}</em></span
     >
-    <span
-      class="badge meta-product"
-      data-bs-toggle="tooltip"
-      data-bs-custom-class="meta-tooltip"
-      data-bs-title="ประกันภัย ประเภท 3+"
-      ><em>ป.3+</em></span
-    >
-    <span
+
+    <!-- <span
       class="badge meta-product meta-include"
       data-bs-toggle="tooltip"
       data-bs-custom-class="meta-tooltip"
       data-bs-title="ป.1 & พ.ร.บ."
       ><em>ควบ</em></span
-    >
+    > -->
     <span
+      v-if="props.row.PaymentType == 'BILL_PAYMENT'"
       class="badge meta-qr"
       data-bs-toggle="tooltip"
       data-bs-custom-class="meta-tooltip"
@@ -100,27 +122,23 @@
       ><em>QR</em></span
     >
     <span
+      v-else-if="props.row.PaymentType == 'CREDIT_CARD'"
       class="badge meta-card"
       data-bs-toggle="tooltip"
       data-bs-custom-class="meta-tooltip"
       data-bs-title="ชำระด้วย บัตรเครดิต/บัตรเดบิต"
       ><em>บัตรเครดิต/บัตรเดบิต</em></span
     >
-    <span
+    <span v-else-if="props.row.PaymentType == 'INSTALLMENT'"
       class="badge meta-installment"
       data-bs-toggle="tooltip"
       data-bs-custom-class="meta-tooltip"
-      data-bs-title="ชำระด้วย บัตรเครดิต/บัตรเดบิต ผ่อน 6 เดือน"
-      ><em>/ 6</em></span
+      :data-bs-title="`ชำระด้วย บัตรเครดิต/บัตรเดบิต ผ่อน ${props.row.NumCredit} เดือน`"
+      ><em>/ {{props.row.NumCredit}}</em></span
     >
+    
     <span
-      class="badge meta-installment-cash"
-      data-bs-toggle="tooltip"
-      data-bs-custom-class="meta-tooltip"
-      data-bs-title="ชำระด้วย เงินสด ผ่อน 12 เดือน"
-      ><em>/ 12</em></span
-    >
-    <span
+      v-else-if="props.row.PaymentType == 'PLEDGE'"
       class="badge meta-pledge"
       data-bs-toggle="tooltip"
       data-bs-custom-class="meta-tooltip"
@@ -128,6 +146,7 @@
       ><em>วงเงินมัดจำ</em></span
     >
     <span
+      v-if="props.row.DiscountType == 'ZERO_COMMISSION'"
       class="badge meta-fulldiscount"
       data-bs-toggle="tooltip"
       data-bs-custom-class="meta-tooltip"
@@ -135,6 +154,7 @@
       ><em>หักเต็ม</em></span
     >
     <span
+      v-else-if="props.row.DiscountType == 'SOME_COMMISSION'"
       class="badge meta-partialdiscount"
       data-bs-toggle="tooltip"
       data-bs-custom-class="meta-tooltip"
@@ -142,6 +162,7 @@
       ><em>หักบางส่วน</em></span
     >
     <span
+      v-else-if="props.row.DiscountType == 'FULL_COMMISSION'"
       class="badge meta-fullpay"
       data-bs-toggle="tooltip"
       data-bs-custom-class="meta-tooltip"
@@ -152,6 +173,127 @@
 </template>
 <script lang="ts" setup>
 import { HistoryResponse } from "~/shared/entities/order-entity";
+
+const OrderType = {
+  C1: {
+    title: "ประเภท 3 Campaign",
+    text: "ประเภท 3 Campaign",
+  },
+  C2: {
+    title: "ประเภท 3 รถใหญ่",
+    text: "ประเภท 3 รถใหญ่",
+  },
+  M1: {
+    title: "ประเภท 1",
+    text: "ประเภท 1",
+  },
+  M2: {
+    title: "ประเภท 2",
+    text: "ประเภท 2",
+  },
+  M3: {
+    title: "ประเภท 3",
+    text: "ประเภท 3",
+  },
+  M4: {
+    title: "ประเภท 4",
+    text: "ประเภท 4",
+  },
+  M5: {
+    title: "ประเภท 5",
+    text: "ประเภท 5",
+  },
+  M9: {
+    title: "ประเภท 9",
+    text: "ประเภท 9",
+  },
+  MOTC: {
+    title: "จักรยานยนต์ (Motor)",
+    text: "จักรยานยนต์ (Motor)",
+  },
+  MPE0: {
+    title: "เรตพิเศษ  0",
+    text: "เรตพิเศษ  0",
+  },
+  MPE1: {
+    title: "เรตพิเศษ 1",
+    text: "เรตพิเศษ 1",
+  },
+  MPE2: {
+    title: "เรตพิเศษ 2",
+    text: "เรตพิเศษ 2",
+  },
+  MPE3: {
+    title: "เรตพิเศษ 3",
+    text: "เรตพิเศษ 3",
+  },
+  P: {
+    title: "พรบ",
+    text: "พรบ",
+  },
+  P0: {
+    title: "พรบ.เรตพิเศษ 0",
+    text: "พรบ.เรตพิเศษ 0",
+  },
+  P1: {
+    title: "พรบ.พิเศษ1 (Taxi)",
+    text: "พรบ.พิเศษ1 (Taxi)",
+  },
+  P2: {
+    title: "พรบ.มอเตอร์ไซต์",
+    text: "พรบ.มอเตอร์ไซต์",
+  },
+  P3: {
+    title: "พรบ.เรตพิเศษ",
+    text: "พรบ.เรตพิเศษ",
+  },
+  P4: {
+    title: "พรบ.รถใหญ่",
+    text: "พรบ.รถใหญ่",
+  },
+};
+const OrderClassType = {
+  M1C1: {
+    title: "ป. 1",
+    text: "ประกันชั้น 1",
+  },
+  M2C1: {
+    title: "ป. 2",
+    text: "ประกันชั้น 2",
+  },
+  M3C1: {
+    title: "ป. 3",
+    text: "ประกันชั้น 3",
+  },
+  M4C1: {
+    title: "ป. 4",
+    text: "ประกันชั้น 4",
+  },
+  M5C1: {
+    title: "ป. 5",
+    text: "ประกันชั้น 5",
+  },
+  M5C2: {
+    title: "ป. 2+ ต.",
+    text: "ประกันชั้น 5 2+ เต็มทุน",
+  },
+  M5C3: {
+    title: "ป. 2+ ธ",
+    text: "ประกันชั้น 5 2+ ธรรมดา",
+  },
+  M5C4: {
+    title: "ป. 3+ ต.",
+    text: "ประกันชั้น 5 3+ เต็มทุน",
+  },
+  M5C5: {
+    title: "ป. 3+ ธ.",
+    text: "ประกันชั้น 5 3+ ธรรมดา",
+  },
+  M2C2: {
+    title: "ป. 2",
+    text: "ประกันชั้น 2",
+  },
+};
 const props = defineProps({
   row: {
     type: Object as () => HistoryResponse,

cf6571d8b1775574f5afc8cc8547e9caca38b2eb
Prasopchok <prasopchokk091@gmail.com>
2023-09-07 08:52:54 +0700

    add component datatable

---
+++ components/order/history/grid/table.vue
@@ -0,0 +1,237 @@
+<template>
+  <div class="card">
+    <div class="card-body card-table">
+      <DataTable
+        id="datatables"
+        class="table table-transaction nowrap"
+        data-order='[[ 2, "asc" ]]'
+        :options="datatableOptions"
+        ref="table"
+      >
+        <thead>
+          <tr>
+            <th data-orderable="false"></th>
+            <th data-orderable="false"></th>
+            <th>เลขที่คำสั่งซื้อ</th>
+            <th data-orderable="false">รายการ</th>
+            <th>จำนวนเงิน (บาท)</th>
+            <th data-orderable="false">ผู้เอาประกัน</th>
+            <th>สถานะ</th>
+            <th class="meta-head" data-orderable="false">รูปแบบการทำรายการ</th>
+          </tr>
+        </thead>
+      </DataTable>
+    </div>
+  </div>
+</template>
+<script lang="ts" setup>
+
+// Define import
+import DataTable from "datatables.net-vue3";
+import DataTablesCore from "datatables.net-bs5";
+import OrderHistoryGridMenu from "~/components/order/history/grid/menu.vue";
+import OrderHistoryGridColumn from "~/components/order/history/grid/column.vue";
+import { renderToString } from "@vue/server-renderer";
+import { Filter } from "~/shared/entities/table-option";
+
+const props = defineProps({
+    filters: {
+    type: Array<Filter>,
+    default: Array<Filter>,
+  },
+});
+
+const emit = defineEmits(["changeTable"])
+const table = ref();
+let dt;
+
+const onLoad = onMounted(async () => {
+  dt = table.value;
+  console.log(dt);
+  emit('changeTable',table.value)
+
+});
+ // DataTable
+ table.value = DataTable.use(DataTablesCore);
+
+// Column options in datatable
+const columns = [
+  {
+    targets: 0,
+    data: "OrderNo",
+    title: "",
+    // render:async function (data, type,row) {
+    //   console.log('type=',type)
+    //   if (type === "display") {
+    //     console.log(row)
+    //     const html = await renderToString(h(OrderHistorySearch))
+    //     console.log(html)
+    //     return html
+    //   }
+
+    //   return data;
+    // },
+  },
+  { data: "OrderNo", title: "", targets: 1 },
+  { data: "OrderNo", title: "เลขที่คำสั่งซื้อ", targets: 2, className: "order" },
+  { data: "OrderGroupType", title: "ผลิตภัณฑ์", targets: 3, className: "subject" },
+  { data: "GrandAmount", title: "จำนวนเงิน (บาท)", targets: 4, className: "amount" },
+  { data: "FirstName", title: "ผู้เอาประกัน", targets: 5, className: "name" },
+  { data: "FirstName", title: "สถานะ", targets: 6, className: "status" },
+  { data: "CreateType", title: "รูปแบบการทำรายการ", targets: 7 },
+
+  // { data: 'office', title: 'Office' },
+  // { data: 'extn', title: 'Extension' },
+  // { data: 'start_date', title: 'Start date' },
+  // { data: 'salary', title: 'Salary' },
+];
+
+// DataTable ajax options
+const token = await useUtility().getToken();
+const datatableAjax = {
+  url: "/api/grid",
+  method: "post",
+  data: (d: any) => {
+    return {
+      ...d,
+      URL: "/Order/grid/history/list",
+      Token: token,
+      Filter: props.filters, //filterOption.value,
+    };
+  },
+};
+
+// DataTable options
+const datatableOptions = {
+  columnDefs: columns,
+
+  processing: true,
+  serverSide: true,
+  ajax: datatableAjax,
+  filter: false,
+  //searchCols: [{}, {}, { search: "My filter" }, { search: "^[0-9]", regex: true }],
+  language: {
+    paginate: {
+      previous: "ก่อนหน้า",
+      next: "ถัดไป",
+    },
+    info: "แสดง _START_ ถึง _END_ จาก _TOTAL_ รายการ",
+    infoEmpty: "ไม่มีรายการ",
+    infoFiltered: "(จากทั้งหมด _MAX_ รายการ)",
+    lengthMenu: "แสดง _MENU_ รายการ",
+    search: "ค้นหา",
+    emptyTable: "ไม่มีรายการ",
+    zeroRecords: "ไม่มีรายการ",
+  },
+  initComplete: function (settings, json) {
+
+  },
+  createdRow: async function (row: any, data: any) {
+    console.log("createdRow [data]=", data);
+    const menu = await renderToString(h(OrderHistoryGridMenu, { row: data }));
+    const has_child = await renderToString(
+      h(OrderHistoryGridColumn, {
+        row: data,
+        field: "has-child",
+        //click: continute,
+      })
+    );
+    const order = await renderToString(
+      h(OrderHistoryGridColumn, {
+        row: data,
+        field: "order",
+      })
+    );
+    const subject = await renderToString(
+      h(OrderHistoryGridColumn, {
+        row: data,
+        field: "subject",
+      })
+    );
+    const amount = await renderToString(
+      h(OrderHistoryGridColumn, {
+        row: data,
+        field: "amount",
+      })
+    );
+    const name = await renderToString(
+      h(OrderHistoryGridColumn, {
+        row: data,
+        field: "name",
+      })
+    );
+    const status = await renderToString(
+      h(OrderHistoryGridColumn, {
+        row: data,
+        field: "status",
+      })
+    );
+    const meta = await renderToString(
+      h(OrderHistoryGridColumn, {
+        row: data,
+        field: "meta",
+      })
+    );
+
+    var tds = row.getElementsByTagName("td");
+    var TdId1 = tds[0];
+    var TdId2 = tds[1]; // has-child
+    var TdId3 = tds[2]; // order
+    var TdId4 = tds[3]; // subject
+    var TdId5 = tds[4]; // amount
+    var TdId6 = tds[5]; // name
+    var TdId7 = tds[6]; // status
+    var TdId8 = tds[7]; // meta
+    TdId1.innerHTML = menu;
+    //TdId2.innerHTML = data.OrderGroupNo != "" ? has_child : "";
+    TdId2.className = data.OrderGroupNo != "" ? "has-child" : "";
+    TdId2.innerHTML = "";
+    TdId3.innerHTML = order;
+    TdId4.innerHTML = subject;
+    TdId5.innerHTML = amount;
+    TdId6.innerHTML = name;
+    TdId7.innerHTML = status;
+    TdId8.innerHTML = meta;
+
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+    const tooltipList = [...tooltipTriggerList].map(
+      (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
+    );
+
+     // Get all the td elements with the class "has-child"
+     const tdHasChildren = document.querySelectorAll('.has-child')
+    // Add a click event listener to each td element
+    tdHasChildren.forEach(function(td) {
+        td.addEventListener('click', function() {
+            // Get the parent tr element
+            var tr = this.parentNode
+
+            // Toggle the class "is-open" on the parent tr element
+            tr.classList.toggle('is-open')
+
+            // Check if the class "is-open" is present after toggling
+            if (tr.classList.contains('is-open')) {
+                // Create the HTML content for the new tr element
+                var newTrHtml = '<tr><td colspan="8" style="background-color: #faefce"><strong>New HTML Here</strong></td></tr>'
+
+                // Insert the new tr element after the current tr
+                tr.insertAdjacentHTML('afterend', newTrHtml)
+            } else {
+                // Check if the next sibling is a tr element
+                if (tr.nextSibling.tagName === 'TR') {
+                    // Remove the next sibling tr element
+                    tr.parentNode.removeChild(tr.nextSibling)
+                }
+            }
+        })
+    })
+  },
+};
+
+
+
+watch(()=>props.filters,()=>{
+console.log('filter in table ',props.filters)
+table.value.dt.draw();
+})
+</script>

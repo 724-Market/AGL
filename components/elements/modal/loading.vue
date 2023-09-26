f93f29d84dae0be94f27596bd2caac6cac5e46b7
Prasopchok <prasopchokk091@gmail.com>
2023-09-26 14:21:27 +0700

    cancel ESC in loading

--- components/elements/modal/loading.vue
+++ components/elements/modal/loading.vue
@@ -1,18 +1,18 @@
 <template>
-    <dialog id="loading-dialog" >
-        <div class="dialog-card loading-card no-icon no-text">
-            <div class="loading-icon">
-                <div class="logo"></div>
-            </div>
-            <div class="loading-loader">
-                <span class="loading-bar">
-                    <span class="loading-progress"></span>
-                </span>
-            </div>
-            <div class="loading-text">กรุณารอสักครู่</div>
-        </div>
-    </dialog>
-    <!-- <div id="dialogModal" class="dialog-modal" v-show="_loading">
+  <dialog id="loading-dialog">
+    <div class="dialog-card loading-card no-icon no-text">
+      <div class="loading-icon">
+        <div class="logo"></div>
+      </div>
+      <div class="loading-loader">
+        <span class="loading-bar">
+          <span class="loading-progress"></span>
+        </span>
+      </div>
+      <div class="loading-text">กรุณารอสักครู่</div>
+    </div>
+  </dialog>
+  <!-- <div id="dialogModal" class="dialog-modal" v-show="_loading">
         <div class="dialog-content modal-content">
             <div class="modal-header">
                 <h1 class="modal-title fs-5" id="exampleModalToggleLabel">แจ้งเตือน</h1>
@@ -31,49 +31,60 @@
 </template>
 
 <script setup lang="ts">
-
 const props = defineProps({
-    loading: Boolean,
+  loading: Boolean,
 });
 
-const _loading = ref(false)
+const _loading = ref(false);
 
 watch(
-    () => props.loading,
-    () => {
-        // console.log('prop value changed', props.loading)
-        if (props.loading) {
-            openModal()
-        }
-        else {
-            closeModal()
-        }
-    }
-)
-const onLoad = onMounted((
-) => {
-    // const myModal = document.getElementById("modal_demo") as Element
-    // modal = new $bootstrap.Modal(myModal);
+  () => props.loading,
+  () => {
+    // console.log('prop value changed', props.loading)
     if (props.loading) {
-        openModal()
+      openModal();
+    } else {
+      closeModal();
     }
+  }
+);
+const onLoad = onMounted(() => {
+  const dialogLoading = document.getElementById("loading-dialog");
+  const openDialogLoading = document.querySelector(".btn-open-loading");
+  if (openDialogLoading) openDialogLoading.addEventListener("click", showDialogLoading);
+  if (dialogLoading) dialogLoading.addEventListener("keydown", handleKeyDown);
 
-})
+  function showDialogLoading() {
+    if (dialogLoading) dialogLoading.showModal();
+  }
+
+  function handleKeyDown(event) {
+    if (event.key === "Escape") {
+      event.preventDefault();
+    }
+    if (event.key === "Alt") {
+      if (dialogLoading) dialogLoading.close();
+    }
+  }
+  // const myModal = document.getElementById("modal_demo") as Element
+  // modal = new $bootstrap.Modal(myModal);
+  if (props.loading) {
+    openModal();
+  }
+});
 function openModal() {
-    //modal.show()
-    _loading.value = props.loading
-    const dialogLoading = document.getElementById('loading-dialog')
-    if(dialogLoading) dialogLoading.showModal()
+  //modal.show()
+  _loading.value = props.loading;
+  const dialogLoading = document.getElementById("loading-dialog");
+  if (dialogLoading) dialogLoading.showModal();
 }
 
 function closeModal() {
-    //modal.hide()
-    _loading.value = false
-    const dialogLoading = document.getElementById('loading-dialog')
-    if(dialogLoading) dialogLoading.close()
+  //modal.hide()
+  _loading.value = false;
+  const dialogLoading = document.getElementById("loading-dialog");
+  if (dialogLoading) dialogLoading.close();
 }
-
-
 </script>
 
 <style scoped>

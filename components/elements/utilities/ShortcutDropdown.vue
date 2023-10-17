d3fb485093db2931611339f33a9f100f4b21302f
Pathompong Sangkajit <p.inhumba@gmail.com>
2023-10-17 11:51:02 +0700

    New: Add template and components

---
+++ components/elements/utilities/ShortcutDropdown.vue
@@ -0,0 +1,79 @@
+<template>
+    <div class="shortcut-dropdown">
+        <a class="monito-dropdown dropdown-toggle no-caret" href="#" role="button" title="ทางลัด" data-bs-toggle="dropdown"
+            aria-expanded="false">
+            <div class="shortcut">
+                <span class="shortcut-text"><span>ทางลัด</span></span>
+            </div>
+        </a>
+        <div class="shortcut-panel dropdown-menu dropdown-menu-dark dropdown-menu-end">
+            <div class="card-shortcut is-success">
+                <div class="shortcut-wrapper">
+                    <div class="shortcut-figure">
+                        <figure class="figure">
+                            <i class="fa-duotone fa-wallet"></i>
+                        </figure>
+                    </div>
+                    <div class="shortcut-info">
+                        <h5 class="topic">เงินมัดจำที่ใช้ได้ <small>(บาท)</small></h5>
+                        <span class="value">18,500</span>
+                    </div>
+                    <div class="shortcut-action">
+                        <button type="button" class="btn-secondary btn-open-wallet"><i
+                                class="fa-solid fa-wallet"></i>เติมเงิน</button>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card-shortcut is-success">
+                <div class="shortcut-wrapper is-stack">
+                    <div class="shortcut-figure">
+                        <figure class="figure">
+                            <i class="fa-duotone fa-scroll fa-swap-opacity"></i>
+                        </figure>
+                    </div>
+                    <div class="shortcut-info">
+                        <h5 class="topic">มูลค่ากระดาษที่ใช้ได้ <small>(บาท)</small></h5>
+                        <span class="value">1,500</span>
+                    </div>
+                    <div class="shortcut-action">
+                        <button type="button" class="btn-white btn-open-papers"><i
+                                class="fa-solid fa-layer-group"></i>คลังกระดาษ</button>
+                        <a class="btn-secondary" href="paper-order-1.html"><i
+                                class="fa-solid fa-file-circle-plus"></i>แลกกระดาษ</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card-shortcut is-warning is-mini">
+                <a href="#">
+                    <div class="shortcut-wrapper">
+                        <div class="shortcut-figure">
+                            <figure class="figure">
+                                <i class="fa-duotone fa-timeline fa-rotate-90"></i>
+                            </figure>
+                        </div>
+                        <div class="shortcut-info">
+                            <h5 class="topic">ติดตามสถานะล่าสุด</h5>
+                        </div>
+                    </div>
+                </a>
+            </div>
+
+            <div class="card-shortcut is-info is-mini">
+                <a href="#">
+                    <div class="shortcut-wrapper">
+                        <div class="shortcut-figure">
+                            <figure class="figure">
+                                <i class="fa-duotone fa-heart-circle-plus fa-swap-opacity"></i>
+                            </figure>
+                        </div>
+                        <div class="shortcut-info">
+                            <h5 class="topic">ต่ออายุสมาชิก</h5>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+</template>
---
+++ components/elements/utilities/UserDropdown.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="user-dropdown">
+        <a class="user-profile monito-dropdown dropdown-toggle" href="#" role="button" title="เครื่องมือจัดการส่วนผู้ใช้งาน"
+            data-bs-toggle="dropdown" aria-expanded="false">
+            <div class="user-info">
+                <figure class="avatar">
+                    <!-- img(src="/uploads/team-5.jpg", alt="") -->
+                </figure>
+                <div class="info">
+                    <h5 class="name">AM00125633</h5>
+                    <span class="level"><i class="fa-duotone fa-sparkles"></i> ระดับ 5</span>
+                </div>
+            </div>
+        </a>
+        <ul class="dropdown-menu dropdown-menu-end">
+            <li class="announcement">ช่วงนี้ฝนตกบ่อย ดูแลสุขภาพด้วยนะครับ</li>
+            <li class="announcement is-warning">สมาชิกจะสิ้นสุด 15/09/2567 <a href="#">คลิกเพื่อต่ออายุสมาชิก</a></li>
+            <li><a class="dropdown-item" href="#"><span class="icon-user">ข้อมูลสมาชิก</span></a></li>
+            <li><a class="dropdown-item" href="#"><span class="icon-gears">การตั้งค่า</span></a></li>
+            <li><a class="dropdown-item" href="#"><span class="icon-gift">สิทธิพิเศษสำหรับคุณ</span></a></li>
+            <li>
+                <hr class="dropdown-divider">
+            </li>
+            <li><a class="dropdown-item" href="#"><span class="icon-calendar-star">กิจกรรม</span></a></li>
+            <li><a class="dropdown-item" href="#"><span class="icon-badge-percent">โปรโมชั่น</span></a></li>
+            <li>
+                <hr class="dropdown-divider">
+            </li>
+            <li><a class="dropdown-item" href="#"><span class="icon-life-ring">แนะนำการใช้งาน</span></a></li>
+            <li><a class="dropdown-item" href="#"><span class="icon-help">ติดต่อเจ้าหน้าที่</span></a></li>
+            <li>
+                <hr class="dropdown-divider">
+            </li>
+            <li><a class="dropdown-item" href="#"><span class="icon-book-law">ข้อกำหนดและเงื่อนไข</span></a></li>
+            <li><a class="dropdown-item" href="#"><span class="icon-user-lock">นโยบายคุ้มครองข้อมูลส่วนบุคคล</span></a></li>
+            <li>
+                <hr class="dropdown-divider">
+            </li>
+            <li><a class="dropdown-item" href="#"><span class="icon-run">ออกจากระบบ</span></a></li>
+        </ul>
+    </div>
+</template>

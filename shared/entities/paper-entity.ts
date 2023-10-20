171bad469289f395023fa837227fa97150dda795
LAPTOP-T524L1MT\acer <hussapol.new@gmail.com>
2023-10-20 20:48:32 +0700

    Add: Create model and repository for API: /Paper/warehouse/by-area/list

--- shared/entities/paper-entity.ts
+++ shared/entities/paper-entity.ts
@@ -2,3 +2,15 @@ export interface AreaListResponse {
     ID: string;
     Name: string;
 }
+
+export interface WarehouseAreaListReq {
+    AreaID: string;
+    Type: string;
+}
+export interface WarehouseAreaListRes {
+    ID: string;
+    Name: string;
+    AreaID: string;
+    AreaName: string;
+    Type: string;
+}
--- shared/repository/paper.ts
+++ shared/repository/paper.ts
@@ -1,12 +1,20 @@
-import { AreaListResponse } from './../entities/paper-entity';
+import { 
+  AreaListResponse,
+  WarehouseAreaListReq,
+  WarehouseAreaListRes
+ } from './../entities/paper-entity';
 import { IAPIResponse } from "../entities/useApi-response";
 
 class PaperModule {
     private RESOURCE = '/Paper';
 
     async getarea(): Promise<IAPIResponse<AreaListResponse[]>> {
-    return await useCallApi().apiRepository<AreaListResponse[]>(`${this.RESOURCE}/area/list`, {})
-  }
+      return await useCallApi().apiRepository<AreaListResponse[]>(`${this.RESOURCE}/area/list`, {})
+    }
+
+    async getWarehouseArea(req: WarehouseAreaListReq): Promise<IAPIResponse<WarehouseAreaListRes[]>> {
+      return await useCallApi().apiRepository<WarehouseAreaListRes[]>(`${this.RESOURCE}/warehouse/by-area/list`, req)
+    }
 }
 
 export default PaperModule;

74e7a9be5cdf131727df47f77ef4a54526f73b4f
zpitsadang.p <zpitsadang.p@pttdigital.com>
2023-11-29 16:09:52 +0700

    remove picker-only

--- pages/order/compulsory/information.vue
+++ pages/order/compulsory/information.vue
@@ -88,14 +88,14 @@
               </div>
               <div class="form-inline">
                 <FormKit type="datepicker" label="เริ่มต้น" name="EffectiveDate" 
-                  placeholder="วัน/เดือน/ปี ค.ศ." format="DD/MM/YYYY" picker-only 
+                  placeholder="วัน/เดือน/ปี ค.ศ." format="DD/MM/YYYY"  
                   :min-date="effectiveMinDate" :max-date="effectiveMaxDate" v-model="effectiveDateText"
                   validation="required" :validation-messages="{ required: 'กรุณากรอกข้อมูล' }" />
               </div>
               
               <div class="form-inline">
                 <FormKit type="datepicker" label="สิ้นสุด" name="ExpireDate" 
-                  placeholder="วัน/เดือน/ปี ค.ศ" format="DD/MM/YYYY" picker-only 
+                  placeholder="วัน/เดือน/ปี ค.ศ" format="DD/MM/YYYY"  
                   :min-date="expireMinDate" :max-date="expireMaxDate" v-model="expireDateText"
                   :disabled="effectiveType == 'FULLYEAR' || effectiveDateText == ''" 
                   validation="required" :validation-messages="{ required: 'กรุณากรอกข้อมูล' }" />

9641e03de70cb975f1b30ea0b14d527d8478c764
Pathompong Sangkajit <p.inhumba@gmail.com>
2024-01-05 11:35:10 +0700

    Fixed: Review code

---
+++ components/papers/history/grid/Actions.vue
@@ -0,0 +1,17 @@
+<template>
+  <a v-if="$props.row" class="btn btn-actions" :href="'/papers/status/' + $props.row.OrderNo" title="ติดตามสถานะ">
+    <span class="fa-regular fa-eye"></span>
+  </a>
+</template>
+  
+<script setup lang="ts">
+// Define import
+import type { PaperHistoryResponse } from "~/shared/entities/paper-entity"
+
+// Define props
+const props = defineProps({
+  row: {
+    type: Object as () => PaperHistoryResponse
+  }
+})
+</script>

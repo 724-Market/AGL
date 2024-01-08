75edb6a9a70b8bfbbde87b522b5f4e5dfa3daba2
Pathompong Sangkajit <p.inhumba@gmail.com>
2024-01-08 12:13:26 +0700

    Refactor: Change modal functions

--- components/elements/dialog/modal.vue
+++ components/elements/dialog/modal.vue
@@ -1,82 +1,110 @@
 <template>
   <Teleport to="body">
-  <dialog id="modal-dialog">
-    <div :class="['dialog-card', 'is-'+$props.modalType]">
-      <!-- Add class 'is-info', 'is-success', 'is-warning', 'is-danger' for color styling -->
-      <div class="card-header">
-        <button type="button" class="btn btn-close btn-close-modal">ปิด</button>
+    <dialog id="modal-dialog">
+      <div :class="['dialog-card', 'is-' + modalTypeClass]">
+        <div class="card-header">
+          <button type="button" class="btn btn-close btn-close-modal" @click="hideModal">ปิด</button>
+        </div>
+        <div class="card-body">
+          <figure v-if="modalTypeClass !== ''" class="dialog-icon">
+            <div :class="['icon', iconClasses[modalTypeClass]]"></div>
+          </figure>
+          <figure v-else class="dialog-icon">
+            <i class="fa-regular fa-thumbs-up"></i>
+          </figure>
+          <h5 v-if="modalTitle">{{ modalTitle }}</h5>
+          <p v-if="modalText">{{ modalText }}</p>
+        </div>
+        <div class="card-footer">
+          <button type="button" class="btn-primary btn-cancel-modal" @click="hideModal">
+            {{ modalButton ? modalButton : 'ตกลง' }}
+          </button>
+        </div>
       </div>
-      <div class="card-body">
-        <figure v-if="$props.modalType == 'success'" class="dialog-icon">
-          <div class="icon check"></div>
-        </figure>
-        <figure v-else-if="$props.modalType == 'danger'" class="dialog-icon">
-          <div class="icon cross"></div>
-        </figure>
-        <figure v-else-if="$props.modalType == 'warning'" class="dialog-icon">
-          <div class="icon exclamation"></div>
-        </figure>
-        <figure v-else-if="$props.modalType == 'info'" class="dialog-icon">
-          <div class="icon question"></div>
-        </figure>
-        <figure v-else class="dialog-icon">
-          <i class="fa-regular fa-thumbs-up"></i>
-        </figure>
-        <h5>{{ $props.modalTitle }}</h5>
-        <p>{{ $props.modalText }}</p>
-      </div>
-      <div class="card-footer">
-        <button type="button" class="btn-primary btn-cancel-modal">ตกลง</button>
-      </div>
-    </div>
-  </dialog>
+    </dialog>
   </Teleport>
 </template>
 
 <script setup>
-
+// Define emit function to emit events
 const emit = defineEmits(['onCloseModal'])
 
+// Define props for the component
 const props = defineProps({
+  isShowModal: Boolean,
   modalType: String,
   modalTitle: String,
   modalText: String,
-  dialogModal: Boolean,
-});
+  modalButton: String,
+})
 
+// Computed property to determine the class based on modalType
+const modalTypeClass = computed(() => {
+  switch (props.modalType) {
+    case 'info':
+      return 'info'
+    case 'warning':
+      return 'warning'
+    case 'success':
+      return 'success'
+    case 'danger':
+      return 'danger'
+    default:
+      return ''
+  }
+})
+
+// Mapping icons class based on modalType 
+const iconClasses = {
+  info: 'question',
+  warning: 'exclamation',
+  success: 'check',
+  danger: 'cross',
+}
+
+// Function to emit the 'onCloseModal' event
+const hideModal = () => emit('onCloseModal')
+
+// on Mounted
 onMounted(() => {
+  // Selecting the close and cancel buttons
+  const closeDialogModal = document.querySelector('.btn-close-modal')
+  const cancelDialogModal = document.querySelector('.btn-cancel-modal')
 
-  const closeDialogModal = document.querySelector(".btn-close-modal");
-  const cancelDialogModal = document.querySelector(".btn-cancel-modal");
+  // Adding click event listeners to the close and cancel buttons
+  closeDialogModal.addEventListener('click', hiddenDialogModal)
+  cancelDialogModal.addEventListener('click', hiddenDialogModal)
 
-  closeDialogModal.addEventListener("click", hiddenDialogModal);
-  cancelDialogModal.addEventListener("click", hiddenDialogModal);
+  // Showing the dialog modal if isShowModal prop is true
+  if (props.isShowModal) showDialogModal()
+})
 
-  if (props.dialogModal) {
-    showDialogModal();
-  }
-
-});
-
+// Function to show the dialog modal
 function showDialogModal() {
-  const dialogModal = document.getElementById("modal-dialog");
-  if (dialogModal) dialogModal.showModal();
+  const dialogModal = document.getElementById('modal-dialog')
+
+  // Check if the dialogModal exists and show it
+  if (dialogModal) dialogModal.showModal()
 }
 
+// Function to hide the dialog modal
 function hiddenDialogModal() {
-  emit('onCloseModal')
-  const dialogModal = document.getElementById("modal-dialog");
-  if (dialogModal) dialogModal.close();
+  const dialogModal = document.getElementById('modal-dialog')
+
+  // Check if the dialogModal exists, close it, and emit the 'onCloseModal' event
+  if (dialogModal) {
+    dialogModal.close()
+    emit('onCloseModal')
+  }
 }
 
-watch(
-  () => props.dialogModal,
-  () => {
-    if (props.dialogModal) {
-      showDialogModal();
-    } else {
-      hiddenDialogModal();
-    }
+// Watcher to detect changes in isShowModal prop
+watch(() => props.isShowModal, () => {
+  // If isShowModal changes, show or hide the modal accordingly
+  if (props.isShowModal) {
+    showDialogModal()
+  } else {
+    hiddenDialogModal()
   }
-);
+})
 </script>
--- pages/register/[id]/index.vue
+++ pages/register/[id]/index.vue
@@ -2,8 +2,8 @@
     <NuxtLayout :name="layout" :layout-class="layoutClass" :page-title="pageTitle" :page-category="pageCategory"
         :show-page-steps="showPageSteps" :show-page-header="showPageHeader">
 
-        <FormKit type="form" @submit="submitRegister" :actions="false" id="form-register" form-class="form-register form-theme"
-            :incomplete-message="false">
+        <FormKit type="form" @submit="submitRegister" :actions="false" id="form-register"
+            form-class="form-register form-theme" :incomplete-message="false">
 
             <div class="row">
                 <div class="col col-main">
@@ -31,8 +31,8 @@
                                     <div class="status-item" v-else>
                                         <h5 class="topic">ระบุผู้แนะนำ</h5>
                                         <p>
-                                            <FormKit type="text" name="AgentCode"
-                                                placeholder="ระบุรหัสผู้แนะนำ" autocomplete="off" />
+                                            <FormKit type="text" name="AgentCode" placeholder="ระบุรหัสผู้แนะนำ"
+                                                autocomplete="off" />
                                         </p>
                                     </div>
                                 </div>
@@ -52,13 +52,8 @@
 
         <ElementsModalLoading :loading="isLoading" />
 
-        <ElementsDialogModal
-            :dialogModal="isDialogModal"
-            :modal-text="messageText"
-            :modal-title="messageTitle"
-            :modal-type="messageType"
-            @on-close-modal="handleCloseModal" 
-        ></ElementsDialogModal>
+        <ElementsDialogModal :isShowModal="isShowModal" :modal-type="modalType" :modal-title="modalTitle"
+            :modal-text="modalText" :modal-button="modalButton" @on-close-modal="handleCloseModal"></ElementsDialogModal>
 
     </NuxtLayout>
 </template>
@@ -72,41 +67,44 @@ const route = useRoute()
 const isLoading = ref(false)
 
 // Modal Dialog
-const isDialogModal = ref(false)
-const messageType = ref('')
-const messageTitle = ref('')
-const messageText = ref('')
+const isShowModal = ref(false)
+const modalType = ref('')
+const modalTitle = ref('')
+const modalText = ref('')
+const modalButton = ref('')
+
 const isAgent = ref(false)
 
 const handleCloseModal = async () => {
-    isDialogModal.value = false
+    isShowModal.value = false
 };
 
 // Submit form event
-const submitRegister = async (formData:any) => {
+const submitRegister = async (formData: any) => {
 
     isLoading.value = true
-    const response  = await useRepository().user.create(formData)
+    const response = await useRepository().user.create(formData)
 
-    if(response.apiResponse.Status == "200") {
+    if (response.apiResponse.Status == "200") {
         isLoading.value = false
-        isDialogModal.value = true
-        messageType.value = 'success'
-        messageTitle.value = 'ลงทะเบียนสำเร็จ'
-        messageText.value = ''
+        isShowModal.value = true
+        modalType.value = 'success'
+        modalTitle.value = 'ลงทะเบียนสำเร็จ'
+        modalText.value = ''
         //response.apiResponse.Data.UserID;
-    } 
+    }
     else {
         isLoading.value = false
-        isDialogModal.value = true
-        messageType.value = 'danger'
-        messageTitle.value = 'ไม่สามารถลงทะเบียนได้'
-        messageText.value = response.apiResponse.ErrorMessage || 'string'
+        isShowModal.value = true
+        modalType.value = 'danger'
+        modalTitle.value = 'ไม่สามารถลงทะเบียนได้'
+        modalButton.value = 'รับทราบ'
+        modalText.value = response.apiResponse.ErrorMessage || 'string'
     }
 
 };
 
-if(route.params.id!='new') {
+if (route.params.id != 'new') {
     // Call Check Agent Info API
     isAgent.value = true
 }

c34ebd8a8b7b162300e37f4f692825c3a8a232a7
LAPTOP-T524L1MT\acer <hussapol.new@gmail.com>
2024-02-07 15:51:12 +0700

    Edit: provincedata format

--- shared/data/provinces-data.ts
+++ shared/data/provinces-data.ts
@@ -1,49 +1,79 @@
 export default [
-    {
-        value: '06C45A0F9043422ABE6FD3F667B00C6E',
-        label: 'สระบุรี'
-    },
-    {
-        value: '083C1C1E463B493AB7F517E489D2B468',
-        label: 'สิงห์บุรี'
-    },
-    {
-        value: '084231C46CAF450A9EC5F2B68991AB7C',
-        label: 'สุราษฎร์ธานี'
-    },
-    {
-        value: '08BF9C591A52464186B4963B11308AF8',
-        label: 'สงขลา'
-    },
-    {
-        value: '08E5AB30A5CA4ED39AAFEDE51EF3DEFD',
-        label: 'ชุมพร'
-    },
-    {
-        value: '0E4B61AD55B447A1816B69CE06005B5F',
-        label: 'สตูล'
-    },
-    {
-        value: '0F2003FE31F5426C818AC3CC480D3342',
-        label: 'จันทบุรี'
-    },
-    {
-        value: '1697724655AC41D4973A0121E768D10C', 
-        label: 'ตาก'
-    }, 
-    { 
-        value: '16D9A966171547D1B025284B3719152C', 
-        label: 'อำนาจเจริญ' 
-    }, 
-    { 
-        value: '23452139330E4F51B7BFDDEEB7217E3D', label: 'พังงา' }, { value: '24B54F3A308D4F41B70C22541EC449A8', label: 'ตรัง' }, { value: '386B4D83AB1A48BA8485775FDE5D58ED', label: 'เชียงราย' }, { value: '386D8E0F26CD47C6B15C9C199AD18D67', label: 'สุโขทัย' }, { value: '3AE79AB1E24C48929ADAC851255CA654', label: 'ยะลา' }, { value: '3FF2A45660F44FAF8465ED4295345013', label: 'นราธิวาส' }, { value: '425C829AB69D438FA0666701865836DC', label: 'ระนอง' }, { value: '43C2DD2FBCE34EF39DC2A2B7BF044414', label: 'พระนครศรีอยุธยา' }, { value: '45CA7B84661448F89542B987D776A69D', label: 'บึงกาฬ' }, { value: '45F3BC35B5194499BB438EAC44BC1C4D', label: 'ลำพูน' }, { value: '46137348107C4BAE86B321430923B606', label: 'นครศรีธรรมราช' }, { value: '4806356B50B84222B78A619012A5408E', label: 'นครราชสีมา' }, { value: '4BA9D88FD49A437FA5B40DE65EBED134', label: 'นครสวรรค์' }, { value: '4D3C4E98A3AE463386DFB08C44071061', label: 'บุรีรัมย์' }, { value: '4DB330BA5E80462DA5C88E19699765EB', label: 'แพร่' }, { value: '4E5C9B0BFE954FB7BE8E20C31DF0C11D', label: 'อ่างทอง' }, { value: '5010C5FF26CA4F8582D7399C4E42733D', label: 'นครปฐม' }, { value: '52452F3356CC49F5975513FCF9A07AB4', label: 'ชัยนาท' }, { value: '56ADF9BFA66940CA9943807B9FF96A53', label: 'กาญจนบุรี' }, { value: '580AEA9CA5BD4210BFA83B6AA4CF357B', label: 'กระบี่' }, { value: '58DC0945E73E4A0F834237100095598D', label: 'ประจวบคีรีขันธ์' }, { value: '5F63DDB544804ABD993E2F054C31A4CC', label: 'หนองบัวลำภู' }, { value: '6162A8EE079C4FF0B944DF4C351987C3', label: 'ชลบุรี' }, { value: '64CB25430DA94C6D8F5848490381414E', label: 'เชียงใหม่' }, { value: '64E26008872B445A812215FCA8DE4026', label: 'ตราด' }, { value: '6D99897769AA478FB68C977C84DBA054', label: 'เลย' }, { value: '7173CB7711D3449AA52F1C25AA383BCE', label: 'ปัตตานี' }, { value: '749DCD1AB29F46F1932A3FC227A1D05A', label: 'นครพนม' }, { value: '74A02FB676204F39941587CA1EA14093', label: 'กรุงเทพมหานคร' }, { value: '74EB5EA71437439982D84BC7BF33A1C7', label: 'ขอนแก่น' }, { value: '75812B77DAC247CFBC13C1EA6B9B185E', label: 'พิจิตร' }, { value: '77CA5C7D78E643C0AA454492457A728C', label: 'ศรีสะเกษ' }, { value: '79F05006172E470E8B6D5A2DCCFE857C', label: 'พัทลุง' }, { value: '7DD21E85289349B794973EB5037BA258', label: 'สกลนคร' }, { value: '86FF9BE44A844F5E92ACF5BEE8FFF72F', label: 'ราชบุรี' }, { value: '897E0AAB819140B4B74F5B35E3D82DC3', label: 'อุตรดิตถ์' }, { value: '8CC9938356C343419F61BA9363F0A8AF', label: 'สระแก้ว' }, { value: '8D192428D73E466FBB9EAAE98EEBE1FA', label: 'ระยอง' }, { value: '8F4630299AFF419CA063F4554990A77A', label: 'อุดรธานี' }, { value: '90CDEE3CD0EC406B99C0E825B755115E', label: 'ลำปาง' }, { value: '91EC11E67B2F457A89F11245648DD3FF', label: 'ภูเก็ต' }, { value: '92AFB742E8424B8CA0D15E144FBC072D', label: 'เพชรบูรณ์' }, { value: '9BB39689B15D4E299FC84E585B249B1C', label: 'มหาสารคาม' }, { value: '9F0A074997264E44B09C91E266AE28C5', label: 'สมุทรปราการ' }, { value: 'AAA8D7922BEC489E9F7FC7DD49B2887A', label: 'สุพรรณบุรี' }, { value: 'ABD0FB89CF634FB994AD999E7C668F31', label: 'กาฬสินธุ์' }, { value: 'AEE683D4C3E547488E2A188F80D522AC', label: 'นนทบุรี' }, { value: 'B221B0F9F1DD405580A534C63DF8EEDD', label: 'ฉะเชิงเทรา' }, { value: 'B3A89417BCD1468F913916861FF0F168', label: 'ปทุมธานี' }, { value: 'B527B6E7D651425BA57F3E6DC2DFDED0', label: 'กำแพงเพชร' }, { value: 'B634E5C770B34374BF53F53FAC6C7511', label: 'พิษณุโลก' }, { value: 'B68E44E35E89477FA5385D961BF6E24D', label: 'นครนายก' }, { value: 'B9F823D029734D1E8AA5FB53DCCBE4D6', label: 'สมุทรสาคร' }, { value: 'BCC72FF59BA9445F840474A6E7B6CF27', label: 'หนองคาย' }, { value: 'C4EB31CCF5B1422EA86814A394084C95', label: 'ลพบุรี' }, { value: 'C9A922E092F44CC59FE6185B7D2F2077', label: 'แม่ฮ่องสอน' }, { value: 'D30DB738D24A46A7B35B6A750B122F71', label: 'ยโสธร' }, { value: 'D337563807AB4B5CB1EAD14B8D556D6C', label: 'น่าน' }, { value: 'D4C0986C94CA4A9C886C9153E4962F22', label: 'เพชรบุรี' }, { value: 'DADDC0B88D804D0EB2FEC1A0FF96D573', label: 'อุทัยธานี' }, { value: 'DB84382C63BE4193A129658BB3524A92', label: 'อุบลราชธานี' }, { value: 'E1AC54A933304404A7EFEC11D91FBBFC', label: 'มุกดาหาร' }, { value: 'E31B11776C77417F998CA08473B207BB', label: 'ชัยภูมิ' }, { value: 'E34F6FCE72A942F98D809BD3C0C92FAB', label: 'พะเยา' }, { value: 'ED3EC3DEF7EE4FE582546AFA85FA3276', label: 'ปราจีนบุรี' }, { value: 'F06C356F8B2E4BD3880AC18FA7F5CF4F', label: 'สุรินทร์' 
-    }, 
-    { 
-        value: 'F0900C7859AB41569736F5FAEA94CE0D', 
-        label: 'สมุทรสงคราม' 
-    }, 
-    {
-        value: 'FC2B936EFACA43C19493553294058E9B', 
-        label: 'ร้อยเอ็ด' 
-    }
+    { value: '06C45A0F9043422ABE6FD3F667B00C6E', label: 'สระบุรี' },
+    { value: '083C1C1E463B493AB7F517E489D2B468', label: 'สิงห์บุรี' },
+    { value: '084231C46CAF450A9EC5F2B68991AB7C', label: 'สุราษฎร์ธานี' },
+    { value: '08BF9C591A52464186B4963B11308AF8', label: 'สงขลา' },
+    { value: '08E5AB30A5CA4ED39AAFEDE51EF3DEFD', label: 'ชุมพร' },
+    { value: '0E4B61AD55B447A1816B69CE06005B5F', label: 'สตูล' },
+    { value: '0F2003FE31F5426C818AC3CC480D3342', label: 'จันทบุรี' },
+    { value: '1697724655AC41D4973A0121E768D10C', label: 'ตาก' },
+    { value: '16D9A966171547D1B025284B3719152C', label: 'อำนาจเจริญ' },
+    { value: '23452139330E4F51B7BFDDEEB7217E3D', label: 'พังงา' },
+    { value: '24B54F3A308D4F41B70C22541EC449A8', label: 'ตรัง' },
+    { value: '386B4D83AB1A48BA8485775FDE5D58ED', label: 'เชียงราย' },
+    { value: '386D8E0F26CD47C6B15C9C199AD18D67', label: 'สุโขทัย' },
+    { value: '3AE79AB1E24C48929ADAC851255CA654', label: 'ยะลา' },
+    { value: '3FF2A45660F44FAF8465ED4295345013', label: 'นราธิวาส' },
+    { value: '425C829AB69D438FA0666701865836DC', label: 'ระนอง' },
+    { value: '43C2DD2FBCE34EF39DC2A2B7BF044414', label: 'พระนครศรีอยุธยา' },
+    { value: '45CA7B84661448F89542B987D776A69D', label: 'บึงกาฬ' },
+    { value: '45F3BC35B5194499BB438EAC44BC1C4D', label: 'ลำพูน' },
+    { value: '46137348107C4BAE86B321430923B606', label: 'นครศรีธรรมราช' },
+    { value: '4806356B50B84222B78A619012A5408E', label: 'นครราชสีมา' },
+    { value: '4BA9D88FD49A437FA5B40DE65EBED134', label: 'นครสวรรค์' },
+    { value: '4D3C4E98A3AE463386DFB08C44071061', label: 'บุรีรัมย์' },
+    { value: '4DB330BA5E80462DA5C88E19699765EB', label: 'แพร่' },
+    { value: '4E5C9B0BFE954FB7BE8E20C31DF0C11D', label: 'อ่างทอง' },
+    { value: '5010C5FF26CA4F8582D7399C4E42733D', label: 'นครปฐม' },
+    { value: '52452F3356CC49F5975513FCF9A07AB4', label: 'ชัยนาท' },
+    { value: '56ADF9BFA66940CA9943807B9FF96A53', label: 'กาญจนบุรี' },
+    { value: '580AEA9CA5BD4210BFA83B6AA4CF357B', label: 'กระบี่' },
+    { value: '58DC0945E73E4A0F834237100095598D', label: 'ประจวบคีรีขันธ์' },
+    { value: '5F63DDB544804ABD993E2F054C31A4CC', label: 'หนองบัวลำภู' },
+    { value: '6162A8EE079C4FF0B944DF4C351987C3', label: 'ชลบุรี' },
+    { value: '64CB25430DA94C6D8F5848490381414E', label: 'เชียงใหม่' },
+    { value: '64E26008872B445A812215FCA8DE4026', label: 'ตราด' },
+    { value: '6D99897769AA478FB68C977C84DBA054', label: 'เลย' },
+    { value: '7173CB7711D3449AA52F1C25AA383BCE', label: 'ปัตตานี' },
+    { value: '749DCD1AB29F46F1932A3FC227A1D05A', label: 'นครพนม' },
+    { value: '74A02FB676204F39941587CA1EA14093', label: 'กรุงเทพมหานคร' },
+    { value: '74EB5EA71437439982D84BC7BF33A1C7', label: 'ขอนแก่น' },
+    { value: '75812B77DAC247CFBC13C1EA6B9B185E', label: 'พิจิตร' },
+    { value: '77CA5C7D78E643C0AA454492457A728C', label: 'ศรีสะเกษ' },
+    { value: '79F05006172E470E8B6D5A2DCCFE857C', label: 'พัทลุง' },
+    { value: '7DD21E85289349B794973EB5037BA258', label: 'สกลนคร' },
+    { value: '86FF9BE44A844F5E92ACF5BEE8FFF72F', label: 'ราชบุรี' },
+    { value: '897E0AAB819140B4B74F5B35E3D82DC3', label: 'อุตรดิตถ์' },
+    { value: '8CC9938356C343419F61BA9363F0A8AF', label: 'สระแก้ว' },
+    { value: '8D192428D73E466FBB9EAAE98EEBE1FA', label: 'ระยอง' },
+    { value: '8F4630299AFF419CA063F4554990A77A', label: 'อุดรธานี' },
+    { value: '90CDEE3CD0EC406B99C0E825B755115E', label: 'ลำปาง' },
+    { value: '91EC11E67B2F457A89F11245648DD3FF', label: 'ภูเก็ต' }, 
+    { value: '92AFB742E8424B8CA0D15E144FBC072D', label: 'เพชรบูรณ์' }, 
+    { value: '9BB39689B15D4E299FC84E585B249B1C', label: 'มหาสารคาม' }, 
+    { value: '9F0A074997264E44B09C91E266AE28C5', label: 'สมุทรปราการ' }, 
+    { value: 'AAA8D7922BEC489E9F7FC7DD49B2887A', label: 'สุพรรณบุรี' }, 
+    { value: 'ABD0FB89CF634FB994AD999E7C668F31', label: 'กาฬสินธุ์' }, 
+    { value: 'AEE683D4C3E547488E2A188F80D522AC', label: 'นนทบุรี' }, 
+    { value: 'B221B0F9F1DD405580A534C63DF8EEDD', label: 'ฉะเชิงเทรา' }, 
+    { value: 'B3A89417BCD1468F913916861FF0F168', label: 'ปทุมธานี' }, 
+    { value: 'B527B6E7D651425BA57F3E6DC2DFDED0', label: 'กำแพงเพชร' }, 
+    { value: 'B634E5C770B34374BF53F53FAC6C7511', label: 'พิษณุโลก' }, 
+    { value: 'B68E44E35E89477FA5385D961BF6E24D', label: 'นครนายก' }, 
+    { value: 'B9F823D029734D1E8AA5FB53DCCBE4D6', label: 'สมุทรสาคร' }, 
+    { value: 'BCC72FF59BA9445F840474A6E7B6CF27', label: 'หนองคาย' }, 
+    { value: 'C4EB31CCF5B1422EA86814A394084C95', label: 'ลพบุรี' }, 
+    { value: 'C9A922E092F44CC59FE6185B7D2F2077', label: 'แม่ฮ่องสอน' }, 
+    { value: 'D30DB738D24A46A7B35B6A750B122F71', label: 'ยโสธร' }, 
+    { value: 'D337563807AB4B5CB1EAD14B8D556D6C', label: 'น่าน' }, 
+    { value: 'D4C0986C94CA4A9C886C9153E4962F22', label: 'เพชรบุรี' }, 
+    { value: 'DADDC0B88D804D0EB2FEC1A0FF96D573', label: 'อุทัยธานี' }, 
+    { value: 'DB84382C63BE4193A129658BB3524A92', label: 'อุบลราชธานี' }, 
+    { value: 'E1AC54A933304404A7EFEC11D91FBBFC', label: 'มุกดาหาร' }, 
+    { value: 'E31B11776C77417F998CA08473B207BB', label: 'ชัยภูมิ' }, 
+    { value: 'E34F6FCE72A942F98D809BD3C0C92FAB', label: 'พะเยา' }, 
+    { value: 'ED3EC3DEF7EE4FE582546AFA85FA3276', label: 'ปราจีนบุรี' }, 
+    { value: 'F06C356F8B2E4BD3880AC18FA7F5CF4F', label: 'สุรินทร์' }, 
+    { value: 'F0900C7859AB41569736F5FAEA94CE0D', label: 'สมุทรสงคราม' }, 
+    { value: 'FC2B936EFACA43C19493553294058E9B', label: 'ร้อยเอ็ด' }
     ]

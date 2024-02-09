4925222a9213bae232542bf78fa12b2b26671579
Pathompong Sangkajit <p.inhumba@gmail.com>
2024-02-09 18:25:52 +0700

    Changed: adjust address modal

--- components/elements/form/IdCard.vue
+++ components/elements/form/IdCard.vue
@@ -1,7 +1,17 @@
 <template>
-  <FormKit type="number" mask="#-####-#####-##-#" placeholder="เลขบัตรประชาชน 13 หลัก"
+  <FormKit type="number" mask="#-####-#####-##-#" v-model="idCard" placeholder="เลขบัตรประชาชน 13 หลัก"
     validation="required|matches:/^[0-9]{13}$/" :validation-messages="{
       required: 'กรุณาใส่เลขบัตรประชาชน',
       matches: 'เลขบัตรประชาชนควรเป็นตัวเลข 13 หลัก'
-    }" show-mask="false" unmask-value="true" inputmode="numeric" autocomplete="off" />
+    }" show-mask="false" unmask-value="true" inputmode="numeric" autocomplete="off" :readonly="props.isReadOnly" />
 </template>
+
+<script setup>
+// Define props for the component
+const props = defineProps({
+  formIdCard: String,
+  isReadOnly: Boolean
+})
+
+const idCard = props.formIdCard
+</script>

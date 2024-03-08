61427b6335ba2264ca4d0faabefe5b74e83dc1a3
Prasopchok <prasopchokk091@gmail.com>
2024-03-08 10:28:47 +0700

    fix api method get set token cookie

--- server/api/aglove.get.ts
+++ server/api/aglove.get.ts
@@ -5,11 +5,16 @@ export default defineEventHandler(async (event) => {
   const query = await getQuery(event)
   const config = useRuntimeConfig()
 
+  const cookieAccessToken = getCookie(event,'access_token');
+  let access_token = query.token != "" ? "Bearer " + query.token : ""
+  if (cookieAccessToken) {
+    access_token = cookieAccessToken
+  }
 
   const response = await $fetch(config.public.BaseUrl + query.url, {
     method: "GET",
     headers: {
-      Authorization: query.token != "" ? "Bearer " + query.token : ""
+      Authorization: access_token
     }
   })
 
